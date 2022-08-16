bae924088c320489b1b1a81d2008a0562ca0db0a
kieu <kieutnhgcs18902@gmail.com>
2022-08-17 00:00:10 +0700

    Add comma,remove comment

--- lib/screen/bookclass.dart
+++ lib/screen/bookclass.dart
@@ -11,76 +11,78 @@ class _BookclassState extends State<Bookclass> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-        child: Column(
-          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  'Dy',
-                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-                    InkWell(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          Image.asset(
-                            'assets/img/schedule.png',
-                            width: 45,
-                            height: 45,
-                          ),
-                          const SizedBox(
-                            width: 15,
-                          ),
-                          const Text('Schedule',
-                              style: TextStyle(
-                                  fontSize: 17, fontWeight: FontWeight.bold))
-                        ],
-                      ),
-                      onTap: () {
-                        Navigator.push(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Dy',
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+                      InkWell(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Image.asset(
+                              'assets/img/schedule.png',
+                              width: 45,
+                              height: 45,
+                            ),
+                            const SizedBox(
+                              width: 15,
+                            ),
+                            const Text('Schedule',
+                                style: TextStyle(
+                                    fontSize: 17, fontWeight: FontWeight.bold))
+                          ],
+                        ),
+                        onTap: () {
+                          Navigator.push(
                             context,
                             MaterialPageRoute(
-                                builder: (context) => Schedule()));
-                      },
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 150,
-            ),
-            Column(
-              children: [
-                Image.asset(
-                  'assets/img/online-presentation.png',
-                  height: 130,
-                  width: 130,
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                const Text(
-                  'Not able to book the class for now,please contact your learning advisor!',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontFamily: 'WorkSans',
+                              builder: (context) => const Schedule(),
+                            ),
+                          );
+                        },
+                      ),
+                    ],
                   ),
-                )
-              ],
-            )
-          ],
+                ],
+              ),
+              const SizedBox(
+                height: 150,
+              ),
+              Column(
+                children: [
+                  Image.asset(
+                    'assets/img/online-presentation.png',
+                    height: 130,
+                    width: 130,
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  const Text(
+                    'Not able to book the class for now,please contact your learning advisor!',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontFamily: 'WorkSans',
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
-    ));
+    );
   }
 }
 
@@ -104,71 +106,75 @@ class _ScheduleState extends State<Schedule> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          actions: <Widget>[
-            Padding(
-                padding: EdgeInsets.only(right: 18),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    const Text(
-                      'Schudle',
-                      style: TextStyle(
-                          fontFamily: 'WorkSans',
-                          fontSize: 25,
-                          color: Colors.black),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        actions: <Widget>[
+          Padding(
+              padding: const EdgeInsets.only(right: 18),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Schudle',
+                    style: TextStyle(
+                        fontFamily: 'WorkSans',
+                        fontSize: 25,
+                        color: Colors.black),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  InkWell(
+                    onTap: _showDatePicker,
+                    child: Image.asset(
+                      'assets/img/schedule.png',
+                      width: 30,
+                      height: 30,
                     ),
-                    const SizedBox(
-                      width: 10,
-                    ),
-                    InkWell(
-                      child: Image.asset(
-                        'assets/img/schedule.png',
-                        width: 30,
-                        height: 30,
-                      ),
-                      onTap: _showDatePicker,
-                    ),
-                  ],
-                )),
-            DropdownButton<String>(
-              value: dropdownValue,
-              items: <String>['All', 'Chinese', 'English', 'Math']
-                  .map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-              onChanged: (String? newValue) {
-                setState(() {
-                  dropdownValue = newValue!;
-                });
-              },
-            )
+                  ),
+                ],
+              )),
+          DropdownButton<String>(
+            value: dropdownValue,
+            items: <String>['All', 'Chinese', 'English', 'Math']
+                .map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+            onChanged: (String? newValue) {
+              setState(() {
+                dropdownValue = newValue!;
+              });
+            },
+          )
+        ],
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 150,
+            ),
+            Image.asset(
+              'assets/img/check-list.png',
+              height: 130,
+              width: 130,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            const Text(
+              'No upcoming class.Book now!',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontFamily: 'WorkSans',
+              ),
+            ),
           ],
         ),
-        body: Center(
-            child: Column(children: [
-          const SizedBox(
-            height: 150,
-          ),
-          Image.asset(
-            'assets/img/check-list.png',
-            height: 130,
-            width: 130,
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          const Text(
-            'No upcoming class.Book now!',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontFamily: 'WorkSans',
-            ),
-          )
-        ])));
+      ),
+    );
   }
 }
